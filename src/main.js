f7e94847e1226ea1f4910f7db9e0bd327d2b1f92
shenoy <vigneshshenoy@gmail.com>
2018-10-22 15:57:45 +0530

    change App.vue to Demo.vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Demo from './Demo.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(Demo)
 }).$mount('#app')
