c464a6d0682a2eb065f36b1459d571271bd960d2
shenoy <vigneshshenoy@gmail.com>
2018-11-05 17:04:05 +0530

    moved build to rollup.js

---
+++ build/rollup.config.js
@@ -0,0 +1,35 @@
+// rollup.config.js
+import vue from 'rollup-plugin-vue';
+import buble from 'rollup-plugin-buble';
+import uglify from 'rollup-plugin-uglify-es';
+import minimist from 'minimist';
+import resolve from 'rollup-plugin-node-resolve';
+import cjs from 'rollup-plugin-commonjs'
+
+const argv = minimist(process.argv.slice(2));
+
+const config = {
+  input: 'src/entry.js',
+  output: {
+    name: 'VueScrollama'
+  },
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    cjs({
+      include: 'node_modules/**',
+    }),
+    vue(),
+    buble()
+  ],
+};
+
+// Only minify browser (iife) version
+if (argv.format === 'iife') {
+  config.plugins.push(uglify());
+}
+
+export default config;
